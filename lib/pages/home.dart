4bd82d6f27fa8801617078e781af61430d6ff472
Amir Begić <amir.begic2019@size.ba>
2022-09-13 15:15:41 +0200

    conditional buttons

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,16 +1,11 @@
-import 'package:booking/model/ProfilUser/profil_response_model.dart';
 import 'package:booking/pages/NavBar/NavBar.dart';
 
-import 'package:booking/pages/NavBar/NavBar2.dart';
-
-import 'package:booking/pages/dobrodosli.dart';
-import 'package:booking/pages/messages.dart';
+import 'package:booking/pages/profilPage.dart';
 import 'package:booking/pages/notifications.dart';
 import 'package:booking/pages/reservations.dart';
 
-import 'package:booking/pages/smjestaji.dart';
 import 'package:booking/pages/Dodavanje%20Hotela/validacija_dodavanja.dart';
-import 'package:booking/services/shared_service.dart';
+
 import 'package:flutter/material.dart';
 
 import 'main_home.dart';
@@ -25,6 +20,7 @@ class GlavnaStranica extends StatefulWidget {
 class _GlavnaStranicaState extends State<GlavnaStranica>
     with TickerProviderStateMixin {
   int currentIndex = 0;
+
   final screens = [
     MainHome(),
     Messages(),
@@ -41,7 +37,7 @@ class _GlavnaStranicaState extends State<GlavnaStranica>
         appBar: AppBar(
           iconTheme: IconThemeData(color: Colors.black),
           centerTitle: true,
-          title: Text(
+          title: const Text(
             "BOOKING",
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -55,31 +51,32 @@ class _GlavnaStranicaState extends State<GlavnaStranica>
         ),
         body: screens[currentIndex],
         bottomNavigationBar: BottomNavigationBar(
-            onTap: (index) => setState(() => currentIndex = index),
-            currentIndex: currentIndex,
-            unselectedItemColor: Color(0xFFB7B7B7),
-            selectedItemColor: Colors.blue,
-            items: [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.account_box),
-                label: ("Home"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.message_outlined),
-                label: ("Poruke"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.add_home_sharp),
-                label: ("Dodaj"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.date_range),
-                label: ("Rezervacije"),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.notifications_active_rounded),
-                label: ("Obavijesti"),
-              ),
-            ]));
+          onTap: (index) => setState(() => currentIndex = index),
+          currentIndex: currentIndex,
+          unselectedItemColor: Color(0xFFB7B7B7),
+          selectedItemColor: Colors.blue,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_box),
+              label: ("Home"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person_add),
+              label: ("Profil"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.add_home_sharp),
+              label: ("Dodaj"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.date_range),
+              label: ("Rezervacije"),
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.notifications_active_rounded),
+              label: ("Obavijesti"),
+            ),
+          ],
+        ));
   }
 }
